429432f6d0699f0eb0a56cbeb14188fc6af57db9
guptanishi <guptanishi1991@gmail.com>
2020-07-07 17:08:39 +0530

    custom function for converting number to words

--- src/main.js
+++ src/main.js
@@ -5,20 +5,12 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import VueGoodTablePlugin from 'vue-good-table';
 
-
 // import the styles 
 import 'vue-good-table/dist/vue-good-table.css';
-
-var converter = require("number-to-words");
 
 Vue.use(VueGoodTablePlugin);
 
 Vue.config.productionTip = false
-
-Vue.filter("toWords", function (value) {
-  if (!value) return "";
-  return converter.toWords(value);
-});
 
 Vue.filter("upperCase", function (value) {
   if (!value) return "";
