6d7d704ea0f703d5e0e1efd623d70cf79f4b83b0
guptanishi <guptanishi1991@gmail.com>
2020-07-12 15:34:28 +0530

    updated css issue in product module

--- src/components/CreateProduct.vue
+++ src/components/CreateProduct.vue
@@ -1,135 +1,142 @@
 <template>
+  <div class="columns container is-fluid">
+    <div class="column is-half">
+      <div class="field">
+        <label class="label">Product Code</label>
+        <div class="control">
+          <input class="input" type="text" placeholder="product code" v-model="productCode" />
+        </div>
+      </div>
 
-<div class="columns container is-fluid">
-    <div class="column is-half "> 
-        <div class="field">
-            <label class="label">Product Code</label>
-            <div class="control">
-                <input class="input" type="text" placeholder="product code" v-model="productCode">
-            </div>
+      <div class="field">
+        <label class="label">Product Name</label>
+        <div class="control has-icons-left has-icons-right">
+          <input class="input" type="text" placeholder="Product Name" v-model="productName" />
         </div>
+      </div>
 
-        <div class="field">
-            <label class="label">Product Name</label>
-            <div class="control has-icons-left has-icons-right">
-                <input class="input" type="text" placeholder="Product Name" v-model="productName" >
-            </div>
+      <div class="field">
+        <label class="label">Unit</label>
+        <div class="control">
+          <div class="select">
+            <select v-model="unit">
+              <option disabled value>Select Value</option>
+              <option>KG</option>
+              <option>Per Piece</option>
+              <option>Bori</option>
+              <option>Bundle</option>
+            </select>
+          </div>
         </div>
+      </div>
+      <div class="field">
+        <label class="label">Cost Price</label>
+        <div class="control has-icons-left has-icons-right">
+          <input class="input" type="Number" placeholder="Cost Price" v-model="costPrice" />
+        </div>
+      </div>
 
-        <div class="field">
-            <label class="label">Unit</label>
-            <div class="control">
-                <div class="select">
-                <select v-model="unit">
-                    <option disabled value="">Select Value</option>
-                    <option>KG</option>
-                    <option>Per Piece</option>
-                </select>
-                </div>
-            </div>
+      <div class="field">
+        <label class="label">Selling Price</label>
+        <div class="control has-icons-left has-icons-right">
+          <input class="input" type="Number" placeholder="Selling Price" v-model="sellingPrice" />
         </div>
-        <div class="field">
-            <label class="label">Cost Price</label>
-            <div class="control has-icons-left has-icons-right">
-                <input class="input" type="Number" placeholder="Cost Price" v-model="costPrice">
-            </div>
-        </div>
+      </div>
 
-        <div class="field">
-            <label class="label">Selling Price</label>
-            <div class="control has-icons-left has-icons-right">
-                <input class="input" type="Number" placeholder="Selling Price" v-model="sellingPrice">
-            </div>
+      <div class="field">
+        <label class="label">Opening Stock</label>
+        <div class="control has-icons-left has-icons-right">
+          <input class="input" type="Number" placeholder="stock" v-model="quantityAvailable" />
         </div>
+      </div>
 
-        <div class="field">
-            <label class="label">Opening Stock</label>
-            <div class="control has-icons-left has-icons-right">
-                <input class="input" type="Number" placeholder="stock" v-model="quantityAvailable">
-            </div>
+      <div class="field">
+        <label class="label">GST %</label>
+        <div class="control has-icons-left has-icons-right">
+          <input class="input" type="text" v-model="gst" />
         </div>
+      </div>
 
-        <div class="field">
-            <label class="label">GST %</label>
-            <div class="control has-icons-left has-icons-right">
-                <input class="input" type="text" v-model="gst">
-            </div>
+      <div class="field">
+        <label class="label">HSN Number</label>
+        <div class="control has-icons-left has-icons-right">
+          <input class="input" type="text" v-model="HSN" />
         </div>
+      </div>
 
-        <div class="field">
-            <label class="label">HSN Number</label>
-            <div class="control has-icons-left has-icons-right">
-                <input class="input" type="text" v-model="HSN">
-            </div>
+      <div class="field is-grouped">
+        <div class="control">
+          <button class="button is-link" v-on:click="create">Submit</button>
         </div>
-
-        <div class="field is-grouped">
-            <div class="control">
-                <button class="button is-link" v-on:click="create">Submit</button>
-            </div>
-            <div class="control">
-                <button class="button is-link is-light">Cancel</button>
-            </div>
+        <div class="control">
+          <button class="button is-link is-light">Cancel</button>
         </div>
+      </div>
     </div>
-</div>
+  </div>
 </template>
 
 <script>
-import {createProduct, getLastProductCode }  from '../repository'
+import { createProduct, getLastProductCode } from "../repository";
 export default {
-  name: 'createProduct',
-  data(){
+  name: "createProduct",
+  data() {
     return {
-      productCode: '',
-      productName: '',
-      unit: '',
-      costPrice: 0.00,
-      sellingPrice: 0.00,
+      productCode: "",
+      productName: "",
+      unit: "",
+      costPrice: 0.0,
+      sellingPrice: 0.0,
       quantityAvailable: 0,
       gst: 0,
-      HSN: '',
-    }
+      HSN: ""
+    };
   },
-  mounted(){
-    getLastProductCode().then(data => {
-        if(data.length == 0){
-            this.productCode = this.generateProductCode(0);
-        }else{
-        
-            let lastnumber= data[0].productCode;
-            let counter = Number(lastnumber.substring(4, lastnumber.length));
-            counter ++; 
-            this.productCode = this.generateProductCode(counter);   
+  mounted() {
+    getLastProductCode()
+      .then(data => {
+        if (data.length == 0) {
+          this.productCode = this.generateProductCode(0);
+        } else {
+          let lastnumber = data[0].productCode;
+          let counter = Number(lastnumber.substring(4, lastnumber.length));
+          counter++;
+          this.productCode = this.generateProductCode(counter);
         }
-        })
-    .catch(err => alert(err.message));
+      })
+      .catch(err => alert(err.message));
   },
 
   methods: {
+    create() {
+      if (this.productCode != "" && this.productName != "") {
+        let data = {
+          productCode: this.productCode,
+          productName: this.productName,
+          unit: this.unit,
+          description: this.description,
+          costPrice: this.costPrice,
+          sellingPrice: this.sellingPrice,
+          quantityAvailable: this.quantityAvailable,
+          gst: this.gst,
+          HSN: this.HSN
+        };
 
-    create(){
-        if(this.productCode != "" && this.productName != ""){
-        let data = { productCode: this.productCode, productName: this.productName , 
-            unit: this.unit, description: this.description, costPrice: this.costPrice, 
-            sellingPrice: this.sellingPrice,quantityAvailable: this.quantityAvailable,
-            gst: this.gst, HSN: this.HSN}
-
-            createProduct(data)
-                .then(data => {
-                    this.$router.push('/');
-                })
-                .catch(err => alert(err.message));
-        }
+        createProduct(data)
+          .then(data => {
+            this.$router.push("/");
+          })
+          .catch(err => alert(err.message));
+      }
     },
 
-    generateProductCode(counter){
-        const prefix = 'LSF';
-        let b  = String(counter).padStart(4, '0')
-        return prefix + '-'+ b;
-    },    
-  },
-}
+    generateProductCode(counter) {
+      const prefix = "LSF";
+      let b = String(counter).padStart(4, "0");
+      return prefix + "-" + b;
+    }
+  }
+};
 </script>
 <style scoped>
 #preview {
--- src/components/LoadProductList.vue
+++ src/components/LoadProductList.vue
@@ -1,101 +1,118 @@
 <template>
-    <div>
-        <center><h1><b>Available Stock Status</b></h1></center>
-        <vue-good-table
-            :columns="columns"
-            :rows="products"
-             @on-row-click="onRowClick"
-            :search-options="{ enabled: true }">
-               <template slot="table-row" slot-scope="props">
-                  <span v-if="props.column.field == 'last'">
-                    <button @click.stop="deleteRow(this, props.row.id)">Delete</button>
-                  </span>
-                  <span v-else>
-                    {{props.formattedRow[props.column.field]}}
-                  </span>
-              </template>
-        </vue-good-table>
-
+  <div>
+    <center>
+      <h1>
+        <b>Products</b>
+      </h1>
+    </center>
+    <div style="margin-left: 20px">
+      <h4>Search products</h4>
     </div>
+    <vue-good-table
+      :columns="columns"
+      :rows="products"
+      @on-row-click="onRowClick"
+      :search-options="{ enabled: true, placeholder: 'Search products', }"
+    >
+      <template slot="table-row" slot-scope="props">
+        <span v-if="props.column.field == 'last'">
+          <button @click.stop="deleteRow(this, props.row.id)">Delete</button>
+        </span>
+        <span v-else>{{props.formattedRow[props.column.field]}}</span>
+      </template>
+    </vue-good-table>
+  </div>
 </template>
 
 <script>
-import { VueGoodTable } from 'vue-good-table';
-import { getProducts, deleteProducts }  from '../repository';
+import { VueGoodTable } from "vue-good-table";
+import { getProducts, deleteProducts } from "../repository";
 export default {
-  name: 'loadproductList',
+  name: "loadproductList",
   components: { VueGoodTable },
-  props:['onModal'],
-  data(){
+  props: ["onModal"],
+  data() {
     return {
       products: [],
-       columns: [
+      columns: [
         {
-          label: 'Product Code',
-          field: 'productCode',
+          label: "Product Code",
+          field: "productCode"
         },
         {
-          label: 'Product Name',
-          field: 'productName',
+          label: "Product Name",
+          field: "productName"
         },
         {
-          label: 'Cost Price',
-          field: 'costPrice',
+          label: "Cost Price",
+          field: "costPrice"
         },
         {
-          label: 'Price',
-          field: 'price',
+          label: "Price",
+          field: "price"
         },
         {
-          label: 'Unit',
-          field: 'unit',
+          label: "Unit",
+          field: "unit"
         },
         {
-          label: 'Vat',
-          field: 'vat',
+          label: "GST",
+          field: "vat"
         },
         {
-          label: 'Quantity Available',
-          field: 'qtyAvailable'
+          label: "Stock Available",
+          field: "qtyAvailable"
         },
         {
-          label: 'HSN',
-          field: 'HSN'
+          label: "HSN",
+          field: "HSN"
         },
         {
-          label: 'Option',
-          field: 'last'
+          label: "Option",
+          field: "last"
         }
-      ],
-    }
+      ]
+    };
   },
-  mounted(){
+  mounted() {
     getProducts()
-      .then(data => this.products = data)
-      .catch((err =>alert(err)));
-    } ,
-    methods: {
-        onRowClick(params){
-          if(!this.onModal)
-            this.$router.push({name:'updateProduct', params: {data: params.row}});
-          else{
-            this.$emit('productRowClicked', params.row);
-          }
-        },
-        deleteRow(event,id){
-           if(confirm('Are you sure you want to delete this product?')){
-              deleteProducts(id)
-                .then(() => {
-                  this.$nextTick(() => {
-                    getProducts()
-                      .then(data => this.products = data)
-                      .catch((err =>alert(err)));
-                  })
-                })
-              .catch((err =>alert(err)));
-           }
-        }
+      .then(data => (this.products = data))
+      .catch(err => alert(err));
+  },
+  methods: {
+    onRowClick(params) {
+      if (!this.onModal)
+        this.$router.push({
+          name: "updateProduct",
+          params: { data: params.row }
+        });
+      else {
+        this.$emit("productRowClicked", params.row);
+      }
+    },
+    deleteRow(event, id) {
+      if (confirm("Are you sure you want to delete this product?")) {
+        deleteProducts(id)
+          .then(() => {
+            this.$nextTick(() => {
+              getProducts()
+                .then(data => (this.products = data))
+                .catch(err => alert(err));
+            });
+          })
+          .catch(err => alert(err));
+      }
     }
-
-}
+  }
+};
 </script>
+
+<style>
+.vgt-global-search {
+  margin-bottom: 20px;
+  margin-top: -40px;
+  width: 55%;
+  margin-right: 30%;
+  float: right;
+}
+</style>
